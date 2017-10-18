bdbcc84632a86bb48d0189503045e3ab9cbf05c1
Lynnik Vlad <lynnik.v@gmail.com>
2017-10-18 12:20:17 +0300

    Added layouts.

---
+++ app/src/main/java/com/lynnik/doodlz/DoodleView.java
@@ -0,0 +1,13 @@
+package com.lynnik.doodlz;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DoodleView extends View {
+
+  public DoodleView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+}
---
+++ app/src/main/java/com/lynnik/doodlz/EraseImageDialogFragment.java
@@ -0,0 +1,5 @@
+package com.lynnik.doodlz;
+
+public class EraseImageDialogFragment {
+
+}
